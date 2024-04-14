62e71e242f10ba799251f838f7193597be4a922d
steinLH <148312124+steinLH@users.noreply.github.com>
2024-04-14 12:03:45 -0700

    Update EventPageController.java

--- controller/EventPageController.java
+++ controller/EventPageController.java
@@ -1,5 +1,64 @@
 package application.controller;
 
+import java.io.IOException;
+
+import application.Main;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import application.Event;
+import javafx.event.ActionEvent;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
 public class EventPageController {
+	
+	private Button rsvpButton;
+	
+	@FXML
+    private void initialize() {	
+	//发起event活动
+	//参加event活动
+	//取消活动
+	//取消参加活动
+	}
+		
+		
+	public void handleRsvp(ActionEvent event) throws Exception {
+        System.out.println("rsvpButton clicked!");
+
+    	// Load the event page
+	    FXMLLoader loader = new FXMLLoader(Main.class.getResource("fxml/rsvpPage.fxml"));
+	    loader.setController(new EventPageController());
+	    Parent root;
+		try {
+			root = loader.load();
+		
+ 		    Scene scene = new Scene(root);
+
+ 		    // Set the stage with the home scene
+ 		    Stage stage = new Stage();
+ 		    stage.setScene(scene);
+ 		    stage.setTitle("rsvp Page");
+ 		    stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    
+    }
 
 }
