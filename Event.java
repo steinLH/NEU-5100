a521c05da5f417cc086d18be76b145be312cb3ab
steinLH <148312124+steinLH@users.noreply.github.com>
2024-04-14 11:51:04 -0700

    Update Event.java

--- Event.java
+++ Event.java
@@ -8,7 +8,12 @@ import java.sql.SQLException;
 import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
 import java.lang.reflect.Field;
 
 public class Event {
@@ -27,9 +32,9 @@ public class Event {
 	public int sponsorPetId;
 	public String sponsorPetName;
 	public int partNum;
-	public String partIdList; //Use commas to separate
-	public String partNameList; //Use commas to separate
-	public String partStatusList; //Use commas to separate // Draft/Active/His/Cancel
+	public String partIdList; //
+	public String partNameList; //
+	public String partStatusList; // Draft/Active/His/Cancel
 	public int likeNum; 
 	public int unLikeNum; 
 	public int favoriteNum; 
@@ -40,10 +45,11 @@ public class Event {
     public static void main(String[] args) throws ParseException, ClassNotFoundException, SQLException {
         
     	Event event = new Event();
+    	Profile profile = new Profile();
     	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     	event.date = dateFormat.parse("2024-03-15 12:30:00"); 
     	// SimpleDateFormat dateFormat2 = new SimpleDateFormat("MMM-dd-YYYY HH:mm"); dateFormat.format(now)
-    	event.eventId = 5;
+    	event.eventId = 4;
     	event.eventTitle = "kara's happy time @moutain view!";
     	event.eventDetail = "On March 20th, Mountain View will host an enchanting gathering where our four-legged friends come together for a special doggy date. Set against the scenic backdrop of the city's parks and green spaces, this event promises a day of tail-wagging excitement and furry camaraderie. From playful pups to wise old souls, dogs of all ages and breeds will converge to socialize, sniff, and strut their stuff. As their human companions mingle, dogs will romp and frolic, exchanging playful barks and friendly sniffs. Attendees can look forward to a variety of activities, including dog-friendly games, agility courses, and perhaps even a doggy fashion show. Throughout the event, local shelters and rescue organizations will be on hand, showcasing adoptable dogs in search of loving forever homes. It's a chance for dog lovers to connect, share stories, and celebrate the unconditional love and joy that our canine companions bring into our lives. So mark your calendars, grab your leashes, and join us for a day filled with laughter, tail wags, and unforgettable moments in the heart of Mountain View.\r\n";
 		event.eventPicUrl = "";
@@ -54,10 +60,10 @@ public class Event {
 		event.sponsorName = "kara2";
 		event.sponsorPetId = 1;
 		event.sponsorPetName = "Wangcai2";
-		event.partNum = 15;
-		event.partIdList ="2,3,5";
-		event.partNameList = "stein,yuga,andy";
-		event.partStatusList = "Draft,Active,Cancel";
+		event.partNum = 4;
+		event.partIdList ="2,3,5,10";
+		event.partNameList = "stein,yuga,andy,laohai";
+		event.partStatusList = " ";
 		event.likeNum = 200;
 		event.favoriteNum = 10;
 		event.unLikeNum = 2;
@@ -73,9 +79,12 @@ public class Event {
             String password = "Stein@123";
             connection = DriverManager.getConnection(url, username, password);
 
-            updateEvent(connection, event);
+            //updateEvent(connection, event);
             getEvent(connection,event);
             //cancelEvent(connection,event);
+            partEvent(connection,event,profile);
+            getEvent(connection,event);
+
 
  
             System.out.print("SQLexe OK");
@@ -91,7 +100,118 @@ public class Event {
     	
     	
     }
+    private static int[] convertToIntArray(String input) {
+        String[] intString = input.split(",");
+        int[] intArray = new int[intString.length];
+        for (int i = 0; i < intString.length; i++) {
+        	intArray[i] = Integer.parseInt(intString[i]);
+        }
+        return intArray;
+    }
+    
+    private static String[] convertToStrArray(String input) {
+        String[] strArray = input.split(",");
+        return strArray;
+    }
+    
+    public static Map getPartList(int partNum,String partIdList,String partNameList) {
+    	Map<Integer,String> hashMap = new HashMap<>();
+    	int[] intArray = convertToIntArray(partIdList);
+    	String[] strArray = convertToStrArray(partNameList);
+    	for(int i=0;i<partNum;i++) {
+    		hashMap.put(intArray[i], strArray[i]);
+    	}
+    	
+        for (Map.Entry<Integer,String> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " 'name is  " + entry.getValue() );
+        }
+        return hashMap;
+    } 
+    public static String[] removePartList(Map<Integer,String> hashMap) {
+    	String[] strArray = {};
+        if (hashMap.containsKey(10)) {
+            System.out.println( " ID is in the map.");
+        }
+        return strArray;
+    } 
 
+    
+	public static void partEvent(Connection connection,Event event,Profile profile) {
+        PreparedStatement selectStatement = null;
+        PreparedStatement updateStatement = null;
+        try {
+        	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        	
+            String selectSql = " SELECT * FROM event WHERE eventid = ? ";
+            selectStatement = connection.prepareStatement(selectSql);
+            selectStatement.setString(1, String.valueOf(event.eventId));
+
+            ResultSet resultSet = selectStatement.executeQuery();
+            if (resultSet.next()) {
+            	// part 
+                System.out.println("SQLexe part");
+                System.out.println("ID: " + resultSet.getInt("eventid"));
+                System.out.println("partnum: " + resultSet.getInt("partnum"));
+                System.out.println("partidlist: " + resultSet.getString("partidlist"));
+                System.out.println("partnamelist: " + resultSet.getString("partnamelist"));
+                int pertNum = resultSet.getInt("partnum");
+                String partIdList = resultSet.getString("partidlist");
+                String partNameList = resultSet.getString("partnamelist");
+                
+
+                String updateSql = "update  event  set  partnum = ?, partidlist = ?, partnamelist = ? "
+            			+ "where eventId = ?";
+                updateStatement = connection.prepareStatement(updateSql);
+                updateStatement.setString(1, Integer.toString(1+event.partNum));
+                updateStatement.setString(2, event.partIdList+",8");
+                updateStatement.setString(3, event.partNameList+",QQ");
+                updateStatement.setString(4, Integer.toString(event.eventId));
+
+                
+                updateStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        
+	}
+	
+	public static void cancelPartEvent(Connection connection,Event event) {
+        PreparedStatement selectStatement = null;
+        PreparedStatement updateStatement = null;
+        try {
+        	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        	
+            String selectSql = " SELECT * FROM event WHERE eventid = ? ";
+            selectStatement = connection.prepareStatement(selectSql);
+            selectStatement.setString(1, String.valueOf(event.eventId));
+
+            ResultSet resultSet = selectStatement.executeQuery();
+            if (resultSet.next()) {
+            	// part 
+                System.out.println("SQLexe part");
+                System.out.println("ID: " + resultSet.getInt("eventid"));
+                System.out.println("partnum: " + resultSet.getInt("partnum"));
+                System.out.println("partidlist: " + resultSet.getString("partidlist"));
+                System.out.println("partnamelist: " + resultSet.getString("partnamelist"));
+
+                String updateSql = "update  event  set  partnum = ?, partidlist = ?, partnamelist = ? "
+            			+ "where eventId = ?";
+                updateStatement = connection.prepareStatement(updateSql);
+                updateStatement.setString(1, Integer.toString(event.partNum-1));
+                updateStatement.setString(2, event.partIdList+",8");
+                updateStatement.setString(3, event.partNameList+",QQ");
+                updateStatement.setString(4, Integer.toString(event.eventId));
+
+                
+                updateStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        
+	}
+    
     //update and insert 
 	public static void updateEvent(Connection connection,Event event) {
         PreparedStatement selectStatement = null;
@@ -149,27 +269,28 @@ public class Event {
             			+ "sponsorpetname, partnum, partidlist, partnamelist, partstatuslist, likenum, unlikenum, favoritenum, createdate,upatedate, status";
                 String insertSql = "INSERT INTO event ("+table_cols+") VALUES (?,?,?,?,?, ?,?,?,?,?, ?,?,?,?,?, ?,?,?,?,?, ?)";
                 insertStatement = connection.prepareStatement(insertSql);
-                insertStatement.setString(1, "zhuli222 pet happy@moutain view park");
-                insertStatement.setString(2, "zhuli222 pet happy  @moutain view park ahahahahahahahahahahahahahahahahahahahahahaha");
+                insertStatement.setString(1, event.eventTitle);
+                insertStatement.setString(2, event.eventDetail);
                 insertStatement.setString(3, "");
-                insertStatement.setString(4, "2024-03-10 12:35:00");
-                insertStatement.setString(5, "11:00:00");
-                insertStatement.setString(6, "14:00:00");
-                insertStatement.setString(7, "Mountain View Park");
-                insertStatement.setString(8, "1" );
-                insertStatement.setString(9, "kara");
-                insertStatement.setString(10, "1");
-                insertStatement.setString(11, "Wangcai");
-                insertStatement.setString(12, "10");
-                insertStatement.setString(13, "2,3");
-                insertStatement.setString(14, "stein,yuga");
-                insertStatement.setString(15, "Active,Active");
-                insertStatement.setString(16, "200");
-                insertStatement.setString(17, "10");
-                insertStatement.setString(18, "2");
-                insertStatement.setString(19, "2024-03-15 12:31:00");
-                insertStatement.setString(20, "2024-03-18 18:31:00");
-                insertStatement.setString(21, "Active");
+                insertStatement.setString(4, "2024-03-10 12:36:00");
+                insertStatement.setString(5, String.valueOf(event.timeStart));
+                insertStatement.setString(6, String.valueOf(event.timeEnd));
+                insertStatement.setString(7, event.place);
+                insertStatement.setString(8, Integer.toString(event.sponsorId) );
+                insertStatement.setString(9, event.sponsorName);
+                insertStatement.setString(10, Integer.toString(event.sponsorPetId) );
+                insertStatement.setString(11, event.sponsorPetName);
+                insertStatement.setString(12, Integer.toString(event.sponsorPetId));
+                insertStatement.setString(13, event.partIdList);
+                insertStatement.setString(14, event.partNameList);
+                insertStatement.setString(15, event.partStatusList);
+                insertStatement.setString(16, Integer.toString(event.likeNum));
+                insertStatement.setString(17, Integer.toString(event.favoriteNum));
+                insertStatement.setString(18, Integer.toString(event.unLikeNum));
+                insertStatement.setString(19, dateFormat.format(event.createDate));
+                insertStatement.setString(20, dateFormat.format(event.upateDate));
+                insertStatement.setString(21, event.status);
+                insertStatement.setString(22, Integer.toString(event.eventId));
         		
                 insertStatement.executeUpdate();
             }
@@ -264,23 +385,13 @@ public class Event {
             
             String selectSql = "SELECT * FROM event WHERE eventid = ?";
             selectStatement = connection.prepareStatement(selectSql);
-            selectStatement.setString(1, "2");
+            selectStatement.setString(1, "4");
 
             ResultSet resultSet = selectStatement.executeQuery();
             if (resultSet.next()) {
             	String table_cols= "eventtitle, eventdetail, eventpicurl, date, timestart, timeend, place, sponsorid, sponsorname,sponsorpetid, "
             			+ "sponsorpetname, partnum, partidlist, partnamelist, partstatuslist, likenum, unlikenum, favoritenum, createdate,upatedate, status";
                 System.out.println("ID: " + resultSet.getInt("eventid"));
-                System.out.println("eventtitle: " + resultSet.getString("eventtitle"));
-                System.out.println("eventdetail: " + resultSet.getString("eventdetail"));
-                System.out.println("date" + resultSet.getDate("date"));
-                System.out.println("timestart: " + resultSet.getTime("timestart"));
-                System.out.println("timeend: " + resultSet.getTime("timeend"));
-                System.out.println("place: " + resultSet.getString("place"));
-                System.out.println("sponsorid: " + resultSet.getInt("sponsorid"));
-                System.out.println("sponsorname: " + resultSet.getString("sponsorname"));
-                System.out.println(" ");
-
                 
                 Field[] fields = event.getClass().getDeclaredFields();
                 for (Field field : fields) {
